e15227eea1318fcbde9de5a86347b86256bcd206
howIdobetter <2079153913@qq.com>
2025-07-25 21:05:27 +0800

    修复了MaxArrayDeque潜在的warning

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -21,7 +21,7 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> {
 
         T maxItem = this.get(0);
         for (int i = 0; i < size(); i++) {
-            if (cmp.compare(maxItem, this.get(i)) > 0) {
+            if (c.compare(maxItem, this.get(i)) > 0) {
                 maxItem = get(i);
             }
         }

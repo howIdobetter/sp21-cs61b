3fa4383dd5d2abb3978f7ab741511c15c009c005
howIdobetter <2079153913@qq.com>
2025-07-30 19:45:49 +0800

    修改了printInOrder的API权限问题

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -164,7 +164,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
         printInOrder(root);
     }
 
-    public void printInOrder(BSTNode x) {
+    private void printInOrder(BSTNode x) {
         if (x == null) {
             return;
         }
